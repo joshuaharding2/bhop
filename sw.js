b35d847947a06f4da2f2133f6beeac263203d155
joshuaharding2 <joshuaharding248@gmail.com>
2026-06-14 16:34:46 -0400

    Update sw.js to look for updated version of files

--- sw.js
+++ sw.js
@@ -25,7 +25,15 @@ self.addEventListener('install', event => {
     );
 });
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        let responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
